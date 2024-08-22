c307f47c4bae48855c6bf20316fc161f7b034079
rocess <kamil@klemczak.pl>
2024-08-23 01:52:34 +0200

    all classes implemented

---
+++ src/main/java/pl/domain/resultchecker/PlayerRepository.java
@@ -0,0 +1,10 @@
+package pl.domain.resultchecker;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlayerRepository {
+    List<Player> saveAll(List<Player> players);
+
+    Optional<Player> findById(String hash);
+}
--- src/main/java/pl/domain/resultchecker/ResultCheckerFacade.java
+++ src/main/java/pl/domain/resultchecker/ResultCheckerFacade.java
@@ -1,4 +1,51 @@
 package pl.domain.resultchecker;
 
+import lombok.AllArgsConstructor;
+import pl.domain.numbergenerator.WinningNumbersGeneratorFacade;
+import pl.domain.numbergenerator.dto.WinningNumbersDto;
+import pl.domain.numberreceiver.NumberReceiverFacade;
+import pl.domain.numberreceiver.dto.TicketDto;
+import pl.domain.resultchecker.dto.PlayerDto;
+import pl.domain.resultchecker.dto.ResultDto;
+
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
 public class ResultCheckerFacade {
+
+    WinningNumbersGeneratorFacade winningNumbersGeneratorFacade;
+    NumberReceiverFacade numberReceiverFacade;
+    PlayerRepository playerRepository;
+    WinnersRetriever winnerGenerator;
+
+    public PlayerDto generateWinners() {
+        List<TicketDto> allTicketsByDate = numberReceiverFacade.retrieveAllTicketsByNextDrawDate();
+        List<Ticket> tickets = ResultCheckerMapper.mapFromTicketDto(allTicketsByDate);
+        WinningNumbersDto winningNumbersDto = winningNumbersGeneratorFacade.generateWinningNumbers();
+        Set<Integer> winningNumbers = winningNumbersDto.getWinningNumbers();
+        if (winningNumbers == null || winningNumbers.isEmpty()) {
+            return PlayerDto.builder()
+                    .message("Winners failed to retrieve")
+                    .build();
+        }
+
+        List<Player> players = winnerGenerator.retreivePlayers(tickets, winningNumbers);
+        playerRepository.saveAll(players);
+        return PlayerDto.builder()
+                .results(ResultCheckerMapper.mapPlayersResults(players))
+                .message("Winner succeeded to retrieve")
+                .build();
+        }
+
+    public ResultDto findByHash(String hash) {
+        Player player = playerRepository.findById(hash).orElseThrow(() -> new RuntimeException("Not found"));
+        return ResultDto.builder()
+                .hash(hash)
+                .numbers(player.numbers())
+                .hitNumbers(player.hitNumbers())
+                .drawDate(player.drawDate())
+                .isWinner(player.isWinner())
+                .build();
+    }
 }
