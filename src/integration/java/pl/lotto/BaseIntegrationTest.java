09ec5559125090aa1a2e11640599847dd491fc5b
rocess <kamil@klemczak.pl>
2024-10-28 21:11:29 +0100

    MongoDB added, pom.xml has been fixed, add docker-compose.yml file. Application start and running without errors.

--- src/integration/java/pl/lotto/BaseIntegrationTest.java
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -14,26 +14,26 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
-import pl.domain.LottoSpringBootApplication;
+import pl.LottoSpringBootApplication;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @SpringBootTest(classes = LottoSpringBootApplication.class)
-@ActiveProfiles("integration") //for the cfg profile
-@AutoConfigureMockMvc
-@Testcontainers
+@ActiveProfiles("integration") //for the cfg profile (after def application.yml next will be application-integration
+@AutoConfigureMockMvc //client for http
+@Testcontainers //it allows to start db only for testing e.g. docker
 public class BaseIntegrationTest {
 
     public static final String WIRE_MOCK_HOST = "http://localhost";
 
-    @Autowired
+    @Autowired //it allows to create http request (annotation here in this section is acceptable)
     public MockMvc mockMvc;
 
-    @Container
-    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+    @Container //database
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.6.1"));
 
     @Autowired
-    public ObjectMapper objectMapper;
+    public ObjectMapper objectMapper; //mapper for JSON objects in java
 
     @RegisterExtension
     public static WireMockExtension wireMockServer = WireMockExtension.newInstance()
--- src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -6,7 +6,7 @@ import pl.lotto.BaseIntegrationTest;
 class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
 
     @Test
-    public void should_user_win_and_system_should generate_winners() {
+    public void should_user_win_and_system_should_generate_winners() {
 //    step 1: user made POST /inputNumbers with 6 numbers (1, 2, 3, 4, 5, 6) at 16-11-2022 10:00 and system returned OK(200) with message: “success” and Ticket (DrawDate:19.11.2022 12:00 (Saturday), TicketId: sampleTicketId)
 //    step 2: system generated winning numbers for draw date: 19.11.2022 12:00
 //    step 3: 3 days and 1 minute passed, and it is 1 minute after the draw date (19.11.2022 12:01)
@@ -16,3 +16,4 @@ class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
     }
 
 }
+
